3a0e629eab46d56bba38175e5653287af96f1d93
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-05-21 16:48:33 +0300

    fix selector

--- src/redax/filterSlice.js
+++ src/redax/filterSlice.js
@@ -2,12 +2,10 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const filterSlice = createSlice({
     name: "filter",
-    initialState: {
-        filter: ""
-    },
+    initialState: '',
     reducers: {
         setFilter(state, action) {
-            state.filter = action.payload
+            return state = action.payload
         }
     }
 });
--- src/redax/selectors.js
+++ src/redax/selectors.js
@@ -1,2 +1,11 @@
-export const getContacts = state => state.contacts.value;
-export const getFilter = state => state.filter.filter;
+export const getContacts = ({ contacts: { value } }) => value;
+export const getFilter = ({ filter }) => filter;
+export const getVisibleContacts = ({ contacts: { value }, filter }) => {
+  if (!filter) {
+    return value;
+  }
+
+  return value.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+};
